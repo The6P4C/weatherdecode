d602122a8318301e0a1491fe8b90bcfb2a398590
the6p4c <me@doggirl.gay>
2016-02-21 18:42:27 +1000

    Move PWM and Bitstream decoders to separate files

---
+++ pwm.py
@@ -0,0 +1,32 @@
+class PWMDecoder:
+	def __init__(self, lengths0 = (61, 65), lengths1 = (19, 24)):
+		self.isOn = False
+		self.onTime = 0
+
+		self.lengths0 = lengths0
+		self.lengths1 = lengths1
+
+	def classify(self, onTime):
+		if onTime >= self.lengths0[0] and onTime <= self.lengths0[1]:
+			return 0
+		elif onTime >= self.lengths1[0] and onTime <= self.lengths1[1]:
+			return 1
+		else:
+			return None;
+
+	def add_samples(self, samples):
+		bitstream = []
+		for sample in samples:
+			if sample > 0.05:
+				if self.isOn:
+					self.onTime += 1
+				else:
+					self.isOn = True
+					self.onTime = 1
+			elif self.isOn:
+				self.isOn = False
+				
+				bitValue = self.classify(self.onTime)
+				if bitValue != None:
+					bitstream.append(bitValue)
+		return bitstream
