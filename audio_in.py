071f44f126bf8c7392d1c32275fb8bce10b059c5
the6p4c <me@doggirl.gay>
2016-02-21 20:14:41 +1000

    Decode from mic/line in audio

---
+++ audio_in.py
@@ -0,0 +1,28 @@
+import pyaudio
+import struct
+
+class AudioIn:
+	def __init__(self, callback):
+		def pa_callback(in_data, frame_count, time_info, status):
+			floats = []
+			for float_bytes in [in_data[i:i + 4] for i in range(0, len(in_data), 4)]:
+		 		floats.append(struct.unpack('f', float_bytes)[0])
+
+			callback(floats)
+
+			return (in_data, pyaudio.paContinue)
+
+		FORMAT = pyaudio.paFloat32
+		CHANNELS = 1
+		RATE = 44100
+
+		self.p = pyaudio.PyAudio()
+
+		self.stream = self.p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, stream_callback=pa_callback)
+
+		self.stream.start_stream()
+
+	def stop(self):
+		self.stream.stop_stream()
+		self.stream.close()
+		self.p.terminate()
--- main.py
+++ main.py
@@ -1,57 +1,43 @@
 import argparse
+from audio_in import AudioIn
 from bitstream import BitstreamDecoder
 from packet import decode_packet
 from pwm import PWMDecoder
-import pyaudio
 from scipy.io import wavfile
-import struct
 import warnings
+import time
 
 def main():
 	parser = argparse.ArgumentParser(description='Decode packets from the Maxkon 433MHz weather station.')
 	parser.add_argument('--file')
 	args = parser.parse_args()
 
+	pwmDecoder = PWMDecoder()
+	bitstreamDecoder = BitstreamDecoder()
+
 	if (args.file):
 		# Suppress WAV file warnings
 		warnings.filterwarnings('ignore')
 		sampFreq, samples = wavfile.read(args.file)
 		warnings.filterwarnings('default')
-
-		pwmDecoder = PWMDecoder()
-		bitstreamDecoder = BitstreamDecoder()
+		
 		bitstream = pwmDecoder.add_samples(samples.tolist())
 		packets = bitstreamDecoder.add_bits(bitstream)
 		
 		for packet in packets:
 			print(decode_packet(packet))
 	else:
-		CHUNK = 1024
-		FORMAT = pyaudio.paFloat32
-		CHANNELS = 1
-		RATE = 44100
-		RECORD_SECONDS = 0.05
+		def audio_samples_ready(samples):
+			bitstream = pwmDecoder.add_samples(samples)
+			packets = bitstreamDecoder.add_bits(bitstream)
 
-		p = pyaudio.PyAudio()
+			for packet in packets:
+				print(decode_packet(packet), flush=True)
 
-		stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+		audioIn = AudioIn(audio_samples_ready)
 
-		print('rec start')
-
-		frames = []
-		for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-			data = stream.read(CHUNK)
-			frames.append(data)
-
-		print('rec end')
-
-		stream.stop_stream()
-		stream.close()
-		p.terminate()
-
-		for frame in frames:
-			for float_bytes in [frame[i:i + 4] for i in range(0, len(frame), 4)]:
-				print(struct.unpack('f', float_bytes)[0])
+		while True:
+			time.sleep(0.1)
 
 if __name__ == '__main__':
 	main()
